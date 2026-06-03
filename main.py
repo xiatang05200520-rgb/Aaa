b1c5c55c28efa887c025b536d46a4cc9239f53fb
xiatang05200520-rgb <xiatang05200520@gmail.com>
2026-06-03 15:57:58 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+top = float(input("請輸入梯形上底長度："))
+bottom = float(input("請輸入梯形下底長度："))
+height = float(input("請輸入梯形高度："))
+
+area = (top + bottom) * height / 2
+
+print("梯形的面積為：", area)
+top = float(input("請輸入梯形上底長度："))
+bottom = float(input("請輸入梯形下底長度："))
+height = float(input("請輸入梯形高度："))
+
+area = (top + bottom) * height / 2
+
+print("梯形的面積為：", area)
